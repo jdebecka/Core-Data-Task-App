f258ccd1cacae5d1dc88967151547f6613543579
Julia Debecka <57728399+jdebecka@users.noreply.github.com>
2020-04-15 18:21:59 +0200

    Initial Commit

---
+++ Core Data Task App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Core Data Task App
+//
+//  Created by Julia Debecka on 15/04/2020.
+//  Copyright © 2020 Julia Debecka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
